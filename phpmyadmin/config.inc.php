eccaa8e45ae70557f56fa922733ca4d13d58cfb8
wiyuga <44395969+wiyuga@users.noreply.github.com>
2019-09-15 00:16:57 +0530

    Update config.inc.php

--- phpmyadmin/config.inc.php
+++ phpmyadmin/config.inc.php
@@ -28,7 +28,7 @@ $i++;
 /* Authentication type */
 $cfg['Servers'][$i]['auth_type'] = 'cookie';
 /* Server parameters */
-$cfg['Servers'][$i]['host'] = 'localhost';
+$cfg['Servers'][$i]['host'] = '203.109.108.78';
 $cfg['Servers'][$i]['compress'] = false;
 $cfg['Servers'][$i]['AllowNoPassword'] = false;
 $cfg['Servers'][$i]['user'] = 'pelliswq_plitrnm';
