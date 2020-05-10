4bcf3536cd885f77a40e595532a60518a96abb84
wikvsprojects <52565615+wikvsprojects@users.noreply.github.com>
2020-05-10 12:35:41 +0530

    Update Home_model.php

--- application/models/Home_model.php
+++ application/models/Home_model.php
@@ -614,12 +614,12 @@ public function forgetpassword($email){
   ini_set('display_errors', 1);
   ini_set('display_startup_errors', 1);
   error_reporting(E_ALL);
-  //$this->db->select('phone');
+  
  $this->db->where('email',$email);
  $query1=$this->db->get('users');    
  $query=$query1->row();
- $query2=$query1->result();
- $mob=$query2->phone;
+ $query2=$query1->result_array();
+ $mob=$query->phone;
  if($query1->num_rows()>0)
  {         
    $this->load->helper('string');
