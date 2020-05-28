a1b0001fe48444e626c4afd77dd5b57f6c99603f
yugandhar <yugandhar@whiteindia.in>
2020-05-28 22:14:07 +0530

    files modified

--- shscripts/cron_push.sh
+++ shscripts/cron_push.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-cd /home/pelliswq/public_html
+cd /var/www/html
 eval $(ssh-agent -s)
-ssh-add ~/.ssh/id_rsa_pelligithub
+ssh-add /root/.ssh/pthoranam
 git add .
 git commit -m "files modified"
 git push --force origin master
