3b9c005f8f37eb9f1645f918482628bb7e0c9f0f
wikvsprojects <52565615+wikvsprojects@users.noreply.github.com>
2020-06-06 11:24:53 +0530

    Update Home_model.php

--- application/models/Home_model.php
+++ application/models/Home_model.php
@@ -342,9 +342,39 @@ class Home_model extends CI_Model {
  //$data = file_get_contents("https://www.google.com/finance/converter?a=$amount&from=$from&to=$to"); 
    //   $data = file_get_contents("https://www.finance.google.com/finance/converter?a=$amount&from=$from&to=$to"); 
     //  preg_match("/<span class=bld>(.*)<\/span>/",$data, $converted); $converted = preg_replace("/[^0-9.]/", "", $converted[1]); 
-     
+    $url=post1('https://openexchangerates.org/api/latest.json?app_id=90757f2fab394122a41c7a34d4e9d7c0');
+    $postStr = http_build_query($postVars);
+    //Create an $options array that can be passed into stream_context_create.
+    $options = array(
+        'http' =>
+            array(
+                'method'  => 'POST', //We are using the POST HTTP method.
+                'header'  => 'Content-type: application/x-www-form-urlencoded',
+                'content' => $postStr //Our URL-encoded query string.
+            )
+    );
+    //Pass our $options array into stream_context_create.
+    //This will return a stream context resource.
+    $streamContext  = stream_context_create($options);
+    //Use PHP's file_get_contents function to carry out the request.
+    //We pass the $streamContext variable in as a third parameter.
+    $result = file_get_contents($url, false, $streamContext);
+    //If $result is FALSE, then the request has failed.
+    if($result === false){
+        //If the request failed, throw an Exception containing
+        //the error.
+        $error = error_get_last();
+        throw new Exception('POST request failed: ' . $error['message']);
+    }
 
-  $result=post1('https://openexchangerates.org/api/latest.json?app_id=90757f2fab394122a41c7a34d4e9d7c0');
+
+
+
+
+
+
+
+ 
   $result=json_decode($result,true);
   //print_r($result);
   $from='USD';
