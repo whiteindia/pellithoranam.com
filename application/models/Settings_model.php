7388e552e8b04d94737ced7067b36bb1bb1d1fbb
wikvsprojects <52565615+wikvsprojects@users.noreply.github.com>
2020-06-09 10:39:01 +0530

    Update Settings_model.php

--- application/models/Settings_model.php
+++ application/models/Settings_model.php
@@ -35,7 +35,8 @@ class Settings_model extends CI_Model {
     //print_r($pass_data['new_password']); 
     //print_r($pass_data['conf_password']);           
       $qry_1 = $this->db->get_where('users', array('user_id'=>$usr->user_id)); // getting password of that user
-      $exist_pass = $this->encrypt->decode($qry_1->result()[0]->password); // decoding pass
+      $exist_pass = $this->encryption->decrypt($qry_1->result()[0]->password); // decoding pass  $exist_pass = $this->encrypt->decode($qry_1->result()[0]->password);
+      //$pass = $this->encryption->decrypt($chk_qry->row()->password);
           echo '<pre>';
     print_r($exist_pass);
     echo '</pre>';
