4bfd051b86366c49b4a2ac2c1422909330853a66
wikvsprojects <52565615+wikvsprojects@users.noreply.github.com>
2020-06-12 21:25:27 +0530

    Update view_changepassword.php

--- admin/application/views/Settings/view_changepassword.php
+++ admin/application/views/Settings/view_changepassword.php
@@ -43,24 +43,3 @@
    </section>
    <!-- /.content -->
 </div>
-<!--
-<div class="modal fade modal-wide" id="popup-subcatgetModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel"
-   aria-hidden="true">
-   <div class="modal-dialog">
-      <div class="modal-content">
-         <div class="modal-header">
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-            <h4 class="modal-title">View Subcategory Details</h4>
-         </div>
-         <div class="modal-subcatbody">
-         </div>
-         <div class="business_info">
-         </div>
-         <div class="modal-footer">
-            <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Close</button>
-         </div>
-      </div>
-      <!-- /.modal-content 
-   </div>-->-->
-   <!-- /.modal-dialog 
-</div>  -->
