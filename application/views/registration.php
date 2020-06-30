ca0f0733678fb9df713cf14ae780c3e77faf0d19
wikvsprojects <52565615+wikvsprojects@users.noreply.github.com>
2020-06-30 09:55:02 +0530

    Update registration.php

--- application/views/registration.php
+++ application/views/registration.php
@@ -413,4 +413,9 @@ $( document ).ready(function() {
 
 });
 </script>
+<script> 
+ $(document).ready(function(){
+    alert('Please fill all the data in the form so that you will have high chance for getting preferred profiles ');
+ });
+</script>
 <?php $this->load->view('forms/register_step_2_js.php');?>
