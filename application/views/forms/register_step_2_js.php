29487b80d0d43e387644b03f95afb94e72984b5a
wikvsprojects <52565615+wikvsprojects@users.noreply.github.com>
2020-06-30 10:35:43 +0530

    Update register_step_2_js.php

--- application/views/forms/register_step_2_js.php
+++ application/views/forms/register_step_2_js.php
@@ -112,8 +112,8 @@
 			{ alert('Family Status can not be empty'); return false; }
 			else if(($('#jo').val() != 'checked') &&($('#nuc').val() != 'checked'))
 			{ alert('Family type can not be empty'); return false; }
-			else if(($('#mid').val() != 'checked') &&($('#up').val() != 'checked')&&($('#rch').val() != 'checked')&&($('#affl').val() != 'checked'))
-			{ alert('Family Status can not be empty'); return false; }
+			else if(($('#ort').val() != 'checked') &&($('#trad').val() != 'checked')&&($('#mod').val() != 'checked')&&($('#lib').val() != 'checked'))
+			{ alert('Family value can not be empty'); return false; }
 			else if($('#about').val() == 0) { alert('Something About You can not be empty'); return false; }
 		    else { }
 
