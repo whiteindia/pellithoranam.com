ebef16334cd7926be0d36532318c7c8b143ceafb
wikvsprojects <52565615+wikvsprojects@users.noreply.github.com>
2020-07-02 12:36:41 +0530

    Update register-step-2.php

--- application/views/forms/register-step-2.php
+++ application/views/forms/register-step-2.php
@@ -174,7 +174,8 @@
                            <div class="row1">
                              <span><input class="wed-reg-input12 reg_input" type="file" name="image" id="image"></span>
                            </div>
-                           <small class="text-danger">** upload jpg,jpeg,png image only.profile picture will be displayed in your profile page once admin approved </small>
+                           <small class="text-danger">** upload jpg,jpeg,png image only.profile picture will be displayed in your profile page once admin approve it </small>
+                           <small class="text-danger">** use standard aspect ratio[ex 4:3 and 5:4.] of your profile picture for better display </small>
                          </div>
                        <div class="clearfix"></div>
                      </li>
