1c91c3ca25b40f501d8ca3abc28fe62ef9cb1aa5
wikvsprojects <52565615+wikvsprojects@users.noreply.github.com>
2020-07-05 23:16:26 +0530

    Update Search.php

--- application/controllers/Search.php
+++ application/controllers/Search.php
@@ -518,7 +518,7 @@ class Search extends CI_Controller {
 
             if((isset($_POST['misc_type']))) {
                 if(in_array("with_photo", $_POST['misc_type'])) { 
-                    $where[]= "profiles.profile_photo <>0"; 
+                    $where[]= "profiles.profile_photo IS NOT NULL"; 
                 }
                  if(in_array("premium", $_POST['misc_type'])) { 
                     $where[]= "profiles.is_premium = '1'";
