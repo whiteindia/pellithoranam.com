99467a40b6fd8d007b43ac55703804c078d25388
wikvsprojects <52565615+wikvsprojects@users.noreply.github.com>
2020-07-09 21:19:15 +0530

    Update register-step-2.php

--- application/views/forms/register-step-2.php
+++ application/views/forms/register-step-2.php
@@ -218,18 +218,19 @@
                       <li>
                        <div class="wed-reg-right-child1 paddingtop10">Upload Profile Photo</div>
                        <div class="wed-reg-right-child2">
-                       <!--image code-->
+
+                           <div class="row1">
+                                                  <!--image code-->
                        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
 
-                        <div>
-                            <img id="user_img"
-                                height="130"
-                                width="130"
-                                style="border:solid" />
-                        </div>
-                       <!--image code    class="wed-reg-input12 reg_input"-->
-                           <div class="row1">
-                             <span><input  type="file" name="image" id="image" onchange="show(this)></span>  
+<div>
+    <img id="user_img"
+        height="130"
+        width="130"
+        style="border:solid" />
+</div>
+   <!--image code-->
+                             <span><input class="wed-reg-input12 reg_input" type="file" name="image" id="image"></span>
                            </div>
                            <small class="text-danger">** upload jpg,jpeg,png image only.profile picture will be displayed in your profile page once admin approve it </small>
                            <small class="text-danger">** use standard aspect ratio[ex 4:3 and 5:4.] of your profile picture for better display </small>
@@ -691,27 +692,3 @@
               margin: 0px 5px;
             }
           </style>
-                       <script>
-function show(input) {
-        debugger;
-        var validExtensions = ['jpg','png','jpeg']; //array of valid extensions
-        var fileName = input.files[0].name;
-        var fileNameExt = fileName.substr(fileName.lastIndexOf('.') + 1);
-        if ($.inArray(fileNameExt, validExtensions) == -1) {
-            input.type = ''
-            input.type = 'file'
-            $('#user_img').attr('src',"");
-            alert("Only these file types are accepted : "+validExtensions.join(', '));
-        }
-        else
-        {
-        if (input.files && input.files[0]) {
-            var filerdr = new FileReader();
-            filerdr.onload = function (e) {
-                $('#user_img').attr('src', e.target.result);
-            }
-            filerdr.readAsDataURL(input.files[0]);
-        }
-        }
-    }
-    </script>
