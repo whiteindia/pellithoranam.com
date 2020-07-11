f46e9d0551bfcbcacfd4b394e717854cc7edf6d6
wikvsprojects <52565615+wikvsprojects@users.noreply.github.com>
2020-07-12 00:23:02 +0530

    Update profiledetail.php

--- application/views/profiledetail.php
+++ application/views/profiledetail.php
@@ -1316,7 +1316,7 @@ if(($sess->matrimony_id==$profile[0]->matrimony_id) || ($sess->gender!=$profile[
             <div class="wed-add-modal-footer">
                  <input class="wed-reg-modal-select" type="text" name="recipient_name"  placeholder="Recipient Name" required>
                  <input class="wed-reg-modal-select" type="text" name="mob_num" placeholder="Recipient Mobile No" required><br/><br/>
-                <textarea class="wed-reg-modal-textarea" cols="50" name="mail_content" readonly>Hi,Please check the profile (PT<?php echo $profile[0]->matrimony_id; ?>) from Pellithoranam.com [https://pellithoranam.com/profile/profile_details/<?php echo $profile[0]->matrimony_id; ?>]</textarea><br/>
+                <textarea class="wed-reg-modal-textarea" cols="50" name="mail_content" readonly>Hi,Please check the profile (PT<?php echo $profile[0]->matrimony_id; ?>) from Pellithoranam.com [ https://pellithoranam.com/profile/profile_details/<?php echo $profile[0]->matrimony_id; ?>]</textarea><br/>
             <!--    <input type="hidden" name="forward_name" value="<?php echo $profile[0]->profile_name; ?>">
                  <input type="hidden" name="profile_photo" value="<?php echo $profile[0]->profile_photo; ?>">
                 <input type="hidden" name="forward_id" value="<?php echo $profile[0]->matrimony_id; ?>">
