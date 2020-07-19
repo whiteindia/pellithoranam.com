756f9113bd28e1ee1a9f90b70837ae911d551686
wikvsprojects <52565615+wikvsprojects@users.noreply.github.com>
2020-07-19 17:10:39 +0530

    Update profiledetail.php

--- application/views/profiledetail.php
+++ application/views/profiledetail.php
@@ -178,7 +178,7 @@ if(($sess->matrimony_id==$profile[0]->matrimony_id) || ($sess->gender!=$profile[
           </div>
 
           <div class="wed-profile-pic-log-detail">
-          <!--   <?php if(!empty($membership)) { if($membership->total_mobileview == 0) { ?>
+          <!--   <?php if(!empty($membership)) { if($membership->total_mobileview > 0) { ?>
            <span data-toggle='modal' data-target='#no_send'>
               <a class="tool_tip" data-toggle="tooltip" data-placement="top"  title="Mobile Number">  
                   <img src="<?php echo base_url(); ?>assets/img/mob.png"> Contact
