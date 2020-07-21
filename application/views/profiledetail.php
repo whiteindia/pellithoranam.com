db0dfd3e74273bce6726f1a837a79351fad758b4
wikvsprojects <52565615+wikvsprojects@users.noreply.github.com>
2020-07-21 21:28:13 +0530

    Update profiledetail.php

--- application/views/profiledetail.php
+++ application/views/profiledetail.php
@@ -526,7 +526,15 @@ if(($sess->matrimony_id==$profile[0]->matrimony_id) || ($sess->gender!=$profile[
                             <div class="child1">Contact Number</div>
                             <div class="child2">:</div>
                             <div class="child3">
-                            <?php if(!empty($membership) && $membership->total_mobileview>0)  echo $membership->total_mobileview.':mv'; ?>
+                            <?php if(!empty($membership) && $membership->total_mobileview>0)  echo $membership->total_mobileview.':mv';
+                            echo '  from'.$this->session->userdata('logged_in')->matrimony_id;
+                            echo '  to'.$profile[0]->matrimony_id;
+                            $query = $this->db->where('mobileview_from',$this->session->userdata('logged_in')->matrimony_id);
+                            $query = $this->db->where('mobileview_to',$profile[0]->matrimony_id); 
+                            $query = $this->db->get('mobile_view'); 
+                             echo 'used'.$query->num_rows();
+                            
+                            ?>
                             <!--
                                 <?php if(!$this->session->userdata('logged_in_admin')) {
                                  if(!empty($membership) && $membership->total_mobileview>0) { ?><strong><span data-toggle="modal" data-target="#view_mob" style="cursor: pointer;">View Number</span></strong><?php } else {?><strong>Locked</strong><?php } ?>
