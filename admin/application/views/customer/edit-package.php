c99fa458f343faf0f38640e01a3a1f6c4f721235
wikvsprojects <52565615+wikvsprojects@users.noreply.github.com>
2020-07-26 22:22:40 +0530

    Update edit-package.php

--- admin/application/views/customer/edit-package.php
+++ admin/application/views/customer/edit-package.php
@@ -63,7 +63,7 @@
  //print_r($package);
  //echo '</pre>'; 
 ?>
-<form></form>
+<form>
     <fieldset>
     <legend><h3>Used Package Info</h3></legend>
 <label>interest</label><?= $package['interest'] ?><br>
@@ -81,6 +81,17 @@
 <label>total_mobileview</label><?= $package['total_mobileview'] ?><br>
 <label>total_sms</label><?= $package['total_sms'] ?><br>
 </fieldset> 
+</form>
+
+<form action="/action_page.php">
+  <fieldset>
+    <legend>Personalia:</legend>
+    <label for="fname">First name:</label><br>
+    <input type="text" id="fname" name="fname" value="John"><br>
+    <label for="lname">Last name:</label><br>
+    <input type="text" id="lname" name="lname" value="Doe"><br><br>
+    <input type="submit" value="Submit">
+  </fieldset>
 </form>
 <div>
 <div class="card text-center">
