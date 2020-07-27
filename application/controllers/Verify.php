34325e830d8a6b0a034d6dca1af2df3039f35fdf
wikvsprojects <52565615+wikvsprojects@users.noreply.github.com>
2020-07-27 19:02:27 +0530

    Update Verify.php

--- application/controllers/Verify.php
+++ application/controllers/Verify.php
@@ -234,9 +234,9 @@ class Verify extends CI_Controller {
       return $uniq;
     }
    public function check_otp() {
-      ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+    //  ini_set('display_errors', 1);
+//ini_set('display_startup_errors', 1);
+//error_reporting(E_ALL);
       $data = $_POST;
      
       // $this->send_email_to_other_user();
