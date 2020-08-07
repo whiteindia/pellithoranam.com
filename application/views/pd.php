7ddabcaca3f199685134ca41904bbc9e90d31320
wikvsprojects <52565615+wikvsprojects@users.noreply.github.com>
2020-08-07 16:02:57 +0530

    Update pd.php

--- application/views/pd.php
+++ application/views/pd.php
@@ -288,11 +288,11 @@ $querym = $this->db->where('mail_from',$this->session->userdata('logged_in')->ma
 				   <?php } ?>
 			  <?php } else {?>
 			  
-              <?php if(!empty($membership)) { if($membership->total_sendmail >0) { ?>
+          <!--     <?php if(!empty($membership)) { if($membership->total_sendmail >0) { ?>
               <input type="button" class="wed-ques-yes" value="Send Mail" data-toggle='modal' data-target='#no_send'/>
               <?php } else { ?>
               <input type="button" class="wed-ques-yes" value="Send Mail" proc_name="<?php echo $profile[0]->profile_name; ?>" matr_id="<?php echo $profile[0]->matrimony_id; ?>" data-toggle='modal' data-target='#send_mail'/>
-              <?php } } ?>  <!---->
+              <?php } } ?> -->
               <?php if(!empty($membership)) { ?>
             <input type="button" class="wed-ques-yes" value="Forward" proc_name="<?php echo $profile[0]->profile_name; ?>" matr_id="<?php echo $profile[0]->matrimony_id; ?>" data-toggle='modal' data-target='#forward'/>
            <?php } ?>
