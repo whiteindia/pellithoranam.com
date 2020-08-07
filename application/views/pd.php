4f24eed253b53c4fd40b1cee9789ce978069f088
wikvsprojects <52565615+wikvsprojects@users.noreply.github.com>
2020-08-07 15:11:10 +0530

    Update pd.php

--- application/views/pd.php
+++ application/views/pd.php
@@ -264,7 +264,7 @@ echo  '--totalmv'.$membership->total_mobileview; */
                             $query = $this->db->where('mail_from',$this->session->userdata('logged_in')->matrimony_id);
                           //  $query = $this->db->where('mobileview_to',$profile[0]->matrimony_id); 
                             $query = $this->db->get('profile_mails'); 
-                             $used=$query->num_rows();
+                             $usedm=$query->num_rows();
 
                              $querym = $this->db->where('matrimony_id',$this->session->userdata('logged_in')->matrimony_id);
                              //  $query = $this->db->where('mobileview_to',$profile[0]->matrimony_id); 
@@ -286,7 +286,7 @@ echo  '--totalmv'.$membership->total_mobileview; */
                            //  if
                             }
                             else{
-                              $used=1;
+                              $usedm=1;
                               //  if
                               $total_mailsent=0;
                             }
@@ -301,11 +301,11 @@ echo  '--totalmv'.$membership->total_mobileview; */
   
   
   
-  	<!--		<?php if(!empty($membership)) { if($membership->total_sendmail == 0) { ?>
+  	<!--	-->	<?php if(!empty($membership)) { if($membership->total_sendmail > 0) { ?>
 					  <input type="button" disabled class="wed-ques-yes" value="Send Mail" data-toggle='modal' data-target='#no_send'/>
 					  <?php } else { ?>
 					  <input type="button" disabled class="wed-ques-yes" value="Send Mail" proc_name="<?php echo $profile[0]->profile_name; ?>" matr_id="<?php echo $profile[0]->matrimony_id; ?>" data-toggle='modal' data-target='#send_mail'/>
-					  <?php } } ?>  -->
+					  <?php } } ?>  
 					  <?php if(!empty($membership)) { ?>
 					<input type="button" disabled  class="wed-ques-yes" value="Forward" proc_name="<?php echo $profile[0]->profile_name; ?>" matr_id="<?php echo $profile[0]->matrimony_id; ?>" data-toggle='modal' data-target='#forward'/>
 				   <?php } ?>
