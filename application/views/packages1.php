fb4d17f942e0cd942deeda5e4d602ecfd162609c
wikvsprojects <52565615+wikvsprojects@users.noreply.github.com>
2020-08-10 11:14:15 +0530

    Update packages1.php

--- application/views/packages1.php
+++ application/views/packages1.php
@@ -448,15 +448,15 @@
               </div>
               <div class="wed-adon-payment-details">
                 <ul  class="payment_option" style="width:100% !important;">
-                  <li class="active " style="width:50%; float:left; text-align:center;" id="razorpay"  onclick="getId('razorpay')">
+                <!--  <li class="active " style="width:50%; float:left; text-align:center;" id="razorpay"  onclick="getId('razorpay')">
                   <a data-toggle="tab" href="#pay" style="width: 100%;float: left;">razorpay</a>
 
                     <div class="arrow-left"></div>
-                  </li>
-                  <li  style="width:50%; float:left; text-align:center;" id="cash" onclick="getId('cash')">
+                  </li>  -->
+             <!--     <li  style="width:50%; float:left; text-align:center;" id="cash" onclick="getId('cash')">
                     <a data-toggle="tab" href="#cod" style="width: 100%;float: left;">CASH</a>
                     <div class="arrow-left"></div>
-                  </li>
+                  </li>  -->
                 </ul>
 				<input type="hidden" name="packageid" id="packageid" >
                   <div class="wed-payment-row"  style="width:100%; float:left; text-align: center; margin-top: 25px;">
