a1ffed8ce25d24177bf3bc7d830e1aa02a86a80c
wikvsprojects <52565615+wikvsprojects@users.noreply.github.com>
2020-08-12 15:35:35 +0530

    Update searchresult.php

--- application/views/searchresult.php
+++ application/views/searchresult.php
@@ -1,11 +1,11 @@
 <?php 
   if(isset($_SESSION['profileverified'])&&$_SESSION['profileverified']==1){
-   if($this->Verify->send_email_to_other_user()){
+   $this->Verify->send_email_to_other_user();
    //$this->verify->functioname();
     //$result= file_get_contents(base_url(verify/send_email_to_other_user));
     echo '<script>alert("alert sent to others");</script>';
     unset($_SESSION['profileverified']);
-   }
+   
   }else {
     echo '<script>alert("in search page");</script>';
   }
