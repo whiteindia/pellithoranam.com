8a879608614c6844b7a357947b43d49cb2e725cb
wikvsprojects <52565615+wikvsprojects@users.noreply.github.com>
2020-08-12 14:42:25 +0530

    Update searchresult.php

--- application/views/searchresult.php
+++ application/views/searchresult.php
@@ -1031,10 +1031,7 @@ $settings= get_setting();
     </div>
   </div>
   <?php 
-  if(isset($_SESSION['profileverified']==1)){
-   // $this->Verify->send_email_to_other_user();
-    $_SESSION['profileverified']==0;
-  }
+  
   
   ?>
 
