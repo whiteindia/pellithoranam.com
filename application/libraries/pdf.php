f6ffe556a4bcb8173fe9a63059dc7bce47b0510c
wikvsprojects <52565615+wikvsprojects@users.noreply.github.com>
2020-09-22 15:34:23 +0530

    Update pdf.php

--- application/libraries/pdf.php
+++ application/libraries/pdf.php
@@ -1,10 +1,10 @@
 
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-require_once dirname(dirname(dirname(__FILE__))). '/tcpdf/tcpdf.php';
+require_once dirname(__FILE__) . '/tcpdf/tcpdf.php';
 class PDF extends TCPDF
 { function __construct() { parent::__construct(); 
-	echo 'dir - <br>';
-echo dirname(__FILE__) . '/tcpdf/';
+//	echo 'dir - <br>';
+//echo dirname(__FILE__) . '/tcpdf/';
 }
 }
 
