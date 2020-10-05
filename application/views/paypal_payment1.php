adbd624de5f6171473be690a1571176bedb27129
wikvsprojects <52565615+wikvsprojects@users.noreply.github.com>
2020-10-05 20:04:04 +0530

    Update paypal_payment1.php

--- application/views/paypal_payment1.php
+++ application/views/paypal_payment1.php
@@ -136,7 +136,7 @@ $filename=$user->matrimony_id.'_'.$t;
 $pdff = $obj_pdf->Output($filename.'.pdf', 'S');
 if(file_put_contents('/var/www/html/assets/uploads/invoices/'.$filename, $pdff)){
 
-    echo 'success'; exit();
+    echo 'success'; // exit();
 }else{
     echo 'failed'; exit();
 
