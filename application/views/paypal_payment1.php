4a5588d69cc9bf064aac504acc5f315a5d422073
wikvsprojects <52565615+wikvsprojects@users.noreply.github.com>
2020-10-05 21:18:41 +0530

    Update paypal_payment1.php

--- application/views/paypal_payment1.php
+++ application/views/paypal_payment1.php
@@ -192,9 +192,9 @@ $config = array(
 //   $mgClient->bcc($bcc);   'attachment' => [ _attachments
 
    $mgClient->from($from,$from_name);
-   $mgClient->subject($subject);
-   $mgClient->message($mailTemplate);
-   $mgClient->attachments(array('filePath'=>'/var/www/html/assets/uploads/invoices/'.$filename, 'filename'=>$filename));
+   $mgClient->subject('Invoice');
+   $mgClient->message($output);
+   $mgClient->attachments('/var/www/html/assets/uploads/invoices/'.$filename);
    if($mgClient->send()){
        echo 'success'; exit();
    }else{
