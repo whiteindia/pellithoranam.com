07471df27d3dfd1832296ebb490167c82b59c207
wikvsprojects <52565615+wikvsprojects@users.noreply.github.com>
2020-10-06 22:23:32 +0530

    Update paypal_payment1.php

--- application/views/paypal_payment1.php
+++ application/views/paypal_payment1.php
@@ -125,7 +125,7 @@ if(file_put_contents('/var/www/html/assets/uploads/invoices/'.$filename, $pdff))
 
 }
 
-
+iconv_set_encoding("internal_encoding", "UTF-8");
 
         $sendMessage = '<html>
                     <body bgcolor="#EDEDEE">
