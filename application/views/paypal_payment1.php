3d31d6d37abdd77fd9f297c4f4b32cc529090a32
wikvsprojects <52565615+wikvsprojects@users.noreply.github.com>
2020-10-06 12:15:43 +0530

    Update paypal_payment1.php

--- application/views/paypal_payment1.php
+++ application/views/paypal_payment1.php
@@ -137,7 +137,74 @@ if(file_put_contents('/var/www/html/assets/uploads/invoices/'.$filename, $pdff))
                   </html>';
                   
  
-/**/
+
+// Recipient 
+$to = 'kvs116.wi@gmail.com'; 
+ 
+// Sender 
+$from = 'noreply@pellithoranam.com'; 
+$fromName = 'CodexWorld'; 
+ 
+// Email subject 
+$subject = 'Invoice attachment';  
+ 
+// Attachment file 
+$file = '/var/www/html/assets/uploads/invoices/'.$filename; //"files/codexworld.pdf"; 
+ 
+// Email body content 
+$htmlContent = ' 
+    <h3>Invoice Attachment</h3> 
+    <p>This email is sent from thepellithoranam with attachment.</p> 
+'; 
+ 
+// Header for sender info 
+$headers = "From: $fromName"." <".$from.">"; 
+ 
+// Boundary  
+$semi_rand = md5(time());  
+$mime_boundary = "==Multipart_Boundary_x{$semi_rand}x";  
+ 
+// Headers for attachment  
+$headers .= "\nMIME-Version: 1.0\n" . "Content-Type: multipart/mixed;\n" . " boundary=\"{$mime_boundary}\""; 
+ 
+// Multipart boundary  
+$message = "--{$mime_boundary}\n" . "Content-Type: text/html; charset=\"UTF-8\"\n" . 
+"Content-Transfer-Encoding: 7bit\n\n" . $htmlContent . "\n\n";  
+ 
+// Preparing attachment 
+if(!empty($file) > 0){ 
+    if(is_file($file)){ 
+        $message .= "--{$mime_boundary}\n"; 
+        $fp =    @fopen($file,"rb"); 
+        $data =  @fread($fp,filesize($file)); 
+ 
+        @fclose($fp); 
+        $data = chunk_split(base64_encode($data)); 
+        $message .= "Content-Type: application/octet-stream; name=\"".basename($file)."\"\n" .  
+        "Content-Description: ".basename($file)."\n" . 
+        "Content-Disposition: attachment;\n" . " filename=\"".basename($file)."\"; size=".filesize($file).";\n" .  
+        "Content-Transfer-Encoding: base64\n\n" . $data . "\n\n"; 
+    } 
+} 
+$message .= "--{$mime_boundary}--"; 
+$returnpath = "-f" . $from; 
+ 
+// Send email 
+$mail = @mail($to, $subject, $message, $headers, $returnpath);  
+ 
+// Email sending status 
+echo $mail?"<h1>Email Sent Successfully!</h1>":"<h1>Email sending failed.</h1>"; 
+
+
+exit();
+
+
+
+
+
+
+
+/**
 $config = array(
     'protocol'  => 'smtp',
     'smtp_host' => 'ssl://smtp.googlemail.com',
@@ -163,7 +230,7 @@ $config = array(
     echo 'Failed';
     echo $this->email->print_debugger();
     exit();
-   }  
+   }  */
 /*
    $this->load->library('Mailgun_lib');
    $mgClient = new Mailgun_lib();
