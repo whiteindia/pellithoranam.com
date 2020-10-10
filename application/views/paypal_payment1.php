7b59d3650b28ee8c66278f6584ed002808f4b8e5
wikvsprojects <52565615+wikvsprojects@users.noreply.github.com>
2020-10-10 21:20:08 +0530

    Update paypal_payment1.php

--- application/views/paypal_payment1.php
+++ application/views/paypal_payment1.php
@@ -247,7 +247,7 @@ $now=date('d-m-Y H:i:s');
         <tr>
           <th><h3>INVOICE TO:</h3>
           <h4>  <br>'.$user->profile_name.' [PT'.$user->matrimony_id.']
-                <br>'.$user->profile_name.'</h4>
+                <br>'.$user->email.'</h4>
           </th>
           <th></th>
           <th></th>
