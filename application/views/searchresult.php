0835365928b3e4960ce13f221600749869f2486f
wikvsprojects <52565615+wikvsprojects@users.noreply.github.com>
2020-10-21 13:03:58 +0530

    Update searchresult.php

--- application/views/searchresult.php
+++ application/views/searchresult.php
@@ -938,8 +938,8 @@ $settings= get_setting();
                                   <?php } } else { ?>
                           <button class="wed-shortlist" data-toggle='modal' data-target='#reglog'>Short List</button>
                         <?php }
-                        <a href="<?php echo base_url(); ?>profile/profile_details/<?php echo $candidate->matrimony_id; ?>" class="btn btn-warning" role="button" style="color:red;font-size:16px;">View Full Profile</a>
-                         if($this->session->userdata('logged_in')) {
+                       ?> <a href="<?php echo base_url(); ?>profile/profile_details/<?php echo $candidate->matrimony_id; ?>" class="btn btn-warning" role="button" style="color:red;font-size:16px;">View Full Profile</a>
+                         <?php if($this->session->userdata('logged_in')) {
                           if((isset($candidate->interested)) && ($candidate->interested == 1)) { ?>
                           <button class="wed-interest">Already Interested</button>
                           <?php } else { ?>
