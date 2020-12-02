eb58466f7804215bf5a5c8905d78eaebffd6c611
wikvsprojects <52565615+wikvsprojects@users.noreply.github.com>
2020-12-02 10:13:39 +0530

    Update Search.php

--- application/controllers/Search.php
+++ application/controllers/Search.php
@@ -834,6 +834,7 @@ ini_set('memory_limit', '-1');
         $data['srch_candidates'] = $this->Search_model->search_user_details($config["per_page"], $page, $where,$or_where,$like);
       
         $data['total_candidates'] = $this->Search_model->search_user_details_count($config["per_page"], $page, $where,$or_where,$like);
+       var_dump($data['total_candidates']);
         $data['photo_count'] = $this->Search_model->photo_count($data['srch_candidates']);
         $data['religions'] = $this->Home_model->getReligions();
         // echo "<pre>";
