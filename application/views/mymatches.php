1c5dccc670467183f031131124635cb10cbccf70
wikvsprojects <52565615+wikvsprojects@users.noreply.github.com>
2020-12-08 15:39:41 +0530

    Update mymatches.php

--- application/views/mymatches.php
+++ application/views/mymatches.php
@@ -772,7 +772,7 @@ img {
                        "> 
                        <div class="col-sm-4">
                        <div class="" style="height:auto;">
-                       <?php echo $candidate->$candidate->profile_id; //echo $l; $l++; srch_candidates $candidate->profile_photo
+                       <?php echo $candidate->profile_id;   //e xcho $l; $l++; srch_candidates $candidate->profile_photo
                        ?>
                       <?php  if($candidate->profile_photo == "")  { 
                           if($this->session->userdata('logged_in')) {
