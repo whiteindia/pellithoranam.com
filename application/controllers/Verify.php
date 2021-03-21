09fb54834ce2b9737a9e7b8626ec16d2570f6e7b
wikvsprojects <52565615+wikvsprojects@users.noreply.github.com>
2021-03-21 19:40:36 +0530

    Update Verify.php

--- application/controllers/Verify.php
+++ application/controllers/Verify.php
@@ -310,6 +310,9 @@ class Verify extends CI_Controller {
 //ini_set('display_errors', 1);
 //ini_set('display_startup_errors', 1);
 //error_reporting(E_ALL);
+ini_set('display_errors', 1);
+  ini_set('display_startup_errors', 1);
+  error_reporting(E_ALL); 
       $this->load->model('Search_model');
       $this->load->model('Home_model');
       $my_matr_id = $this->session->userdata('logged_in');
