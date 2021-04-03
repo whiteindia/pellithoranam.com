3a3da57615128fce2b112bf1fde518dbb4aedb2a
wikvsprojects <52565615+wikvsprojects@users.noreply.github.com>
2021-04-03 23:10:23 +0530

    Update mymatches.php

--- application/views/mymatches.php
+++ application/views/mymatches.php
@@ -1233,3 +1233,25 @@ $(".photo_already_request_btn").on("click",function(e){
           </div>
         </div>
       </div>
+      <script>
+        //When the page has loaded.
+        $( document ).ready(function(){
+            //Perform Ajax request.
+            $.ajax({
+                url: '<?php echo base_url('Verify/send_email_to_other_user'); ?>',
+                type: 'get',
+                async:true,
+                success: function(data){
+                    //If the success function is executed,
+                    //then the Ajax request was successful.
+                    //Add the data we received in our Ajax
+                    //request to the "content" div.
+                    $('#content').html(data);
+                },
+                error: function (xhr, ajaxOptions, thrownError) {
+                    var errorMsg = 'Ajax request failed: ' + xhr.responseText;
+                    $('#content').html(errorMsg);
+                  }
+            });
+        });
+        </script>
